3d18299207f6413610d1e98c0d13db2f762153c2
RobertoJudet <roberto@syneto.eu>
2020-11-11 21:26:20 +0200

    Add comments to initial tests example

--- tests.py
+++ tests.py
@@ -1,11 +1,13 @@
 # first we need to import the library
 import unittest
 
+# we need to create a class and extend TestCase
 class TestUser(unittest.TestCase):
   def test_true(self):
     self.assertEqual(True, True)
 
   def test_false(self):
+    # we can add a message in case of failure
     self.assertEqual(True, False, 'error, True is not equal to False')
 
   def test_new(self):
