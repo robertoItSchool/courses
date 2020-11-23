7000a8de78a97b855ac27ef1c3bc515b7cdc1479
RobertoJudet <roberto@syneto.eu>
2020-11-23 20:57:28 +0200

    Add objects for credentials info

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-list_users = ['roberto', 'roberto', 'roberto']
-list_websites = ['amazon', 'netflix', 'spotify']
-list_passwords = ['123456', 'abcdefg', 'a12abeee']
+class Credentials:
+  # we need to give an user & password
+  def __init__(self, user, password):
+    # save the variables to the object
+    self.user = user
+    self.password = password
+
+
+# extent Credentials
+class EmptyCredentials(Credentials):
+  def __init__(self):
+    # call the parent (Credentials) constructor
+    super().__init__('', None)
+
 
 dict = {
-  'amazon': {'user': 'roberto', 'password': '123456'},
-  'netflix': {'user': 'roberto', 'password': 'abcdefg'},
-  'spotify': {'user': 'roberto', 'password': 'a12abeee'}
+  'amazon': Credentials('roberto', '123456'),
+  'netflix': Credentials('roberto', 'abcdefg'),
+  'spotify': Credentials('roberto', 'ab12eeee')
 }
 
 
 def get_user_and_password(website_name):
   try:
-    user = dict[website_name]['user']
-    password = dict[website_name]['password']
-    return user, password
+    return dict[website_name]
   # if an exception of type KeyError occurs, execute the code in except
   except KeyError:
-    return '', ''
+    return EmptyCredentials()
 
 
+if __name__ == '__main__':
+  new = Credentials('value1', 'value2')
+  print(new.__dict__)
 
-for website in list_websites:
-  print(get_user_and_password(website))
--- tests.py
+++ tests.py
@@ -1,18 +1,21 @@
 import unittest
-from main import get_user_and_password
+from main import get_user_and_password, EmptyCredentials, Credentials
 
 
 class TestPassword(unittest.TestCase):
   def test_get_user_and_password(self):
-    user, password = get_user_and_password('netflix')
+    credentials = get_user_and_password('netflix')
 
-    self.assertEqual('roberto', user)
-    self.assertEqual('abcdefg', password)
+    expected_credentials = Credentials('roberto', 'abcdefg')
+    self.assertEqual(expected_credentials.__dict__, credentials.__dict__)
 
   def test_get_not_configured_website(self):
     # execute - try to get info for a site not in the dictionary
-    user, password = get_user_and_password('emag')
+    credentials = get_user_and_password('emag')
 
     # assertion - expect the values to be empty strings
-    self.assertEqual('', user)
-    self.assertEqual('', password)
+    self.assertEqual(EmptyCredentials().__dict__, credentials.__dict__)
+
+
+if __name__ == '__main__':
+  unittest.main()
