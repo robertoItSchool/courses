5b7143a0d8d834ef13bec7680eb05ec71e01b4f3
Roberto <roberto.judet@itschool.ro>
2021-01-21 11:05:04 +0200

    Add post, put, delete

--- client.py
+++ client.py
@@ -1,7 +1,17 @@
 import requests
+import json
 
-url = 'http://localhost:8000/hello'
-response = requests.get(url)
+# url = 'http://localhost:8000/hello'
+# response = requests.get(url)
+#
+# print(response)
+# print(response.content)
+#
+# url_post = 'http://localhost:8000/file'
+# data = {'file_name': 'un_nume_de_fisier'}
+# headers = {'file_name': 'fisier'}
+# response = requests.post(url_post, json.dumps(data), headers=headers)
 
-print(response)
-print(response.content)
+url_put = 'http://localhost:8000/file'
+headers = {'file_name': 'fisier', 'content': 'hello'}
+requests.delete(url_put, headers=headers)
--- request_handler.py
+++ request_handler.py
@@ -1,12 +1,54 @@
 from http.server import BaseHTTPRequestHandler
+import os
 
 
 class OurHandler(BaseHTTPRequestHandler):
   def do_GET(self):
-    if self.path == 'hello':
-      # put the status code 200 (means ok)
-      self.send_response(200)
-      # end the header space
-      self.end_headers()
+    print('get request')
+    self.__get_hello()
+
+  def do_POST(self):
+    print('post request')
+    self.__post_file()
+
+  def do_PUT(self):
+    print('put request')
+    if self.path == '/file':
+      print('file endpoint')
+      file_name = self.headers.get('file_name')
+      file = open(file_name, 'w')
+      content = self.headers.get('content')
+      file.write(content)
+      file.close()
+      self.__send_200_response()
+
+  def do_DELETE(self):
+    print('delete request')
+    if self.path == '/file':
+      file_name = self.headers.get('file_name')
+      os.remove(file_name)
+      self.__send_200_response()
+
+  def __post_file(self):
+    if self.path == '/file':
+      print('file endpoint')
+      file_name = self.headers.get('file_name')
+      print('file name: ' + file_name)
+      file = open(file_name, 'w')
+      file.close()
+      self.__send_200_response()
+
+  def __get_hello(self):
+    if self.path == '/hello':
+      print('hello request')
+      self.__send_200_response()
       # write the body (response.content)
       self.wfile.write(b'Hello world!')
+
+  def __send_200_response(self):
+    # put the status code 200 (means ok)
+    self.send_response(200)
+    # start the header space
+    self.send_header('Content-type', 'application/json')
+    # end the header space
+    self.end_headers()
