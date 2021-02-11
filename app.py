e6b886b72b7c267d4af36a545b9df655da549045
Roberto <roberto.judet@itschool.ro>
2021-02-11 15:43:56 +0200

    Create UI for adding a task and repo method for deleting a task

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import flask
 # .. = the above folder
 # from .. import repository
 from repository import TaskRepository
+from task import Task
 
 server = flask.Flask(__name__)
 
@@ -12,8 +13,15 @@ def hello():
   return 'Hello world!'
 
 
-# define what to show when accessing the server/
+# define the menu root page
 @server.route('/')
+def menu():
+  template = flask.render_template('menu.html')
+  return template
+
+
+# define what to show when accessing the server/
+@server.route('/list')
 def main():
   repo = TaskRepository()
   tasks = repo.get()
@@ -22,3 +30,16 @@ def main():
   html_template = flask.render_template('hello.html', p=paragraph, tasks=tasks)
   print(html_template)
   return html_template
+
+
+@server.route('/new', methods=('GET', 'POST'))
+def new():
+  if flask.request.method == 'GET':
+    return flask.render_template('new.html')
+  if flask.request.method == 'POST':
+    new_task = Task(flask.request.form['task-name'],
+                    flask.request.form['task-type'])
+    repo = TaskRepository()
+    repo.add(new_task)
+    return flask.redirect(flask.url_for('main'))
+
--- repository.py
+++ repository.py
@@ -23,14 +23,19 @@ class TaskRepository:
     #   tasks.append(to_object)
     return tasks
 
+  def delete(self, task: Task):
+    list_of_tasks = self.__read_from_file()
+    tasks = [x for x in list_of_tasks if x['name'] != task.name]
+    self.__write_to_file(tasks)
+
   def __add_task_info(self, list_of_tasks, task):
     # extract the info from the Task object
     list_of_tasks.append(TaskMapper.to_dict(task))
+    return list_of_tasks
+
+  def __write_to_file(self, list_of_tasks):
     # create a JSON
     file_content = json.dumps(list_of_tasks)
-    return file_content
-
-  def __write_to_file(self, file_content):
     # open the file for writing, create it if it does not exist
     file = open(self.file_name, 'w')
     # save it to the file
@@ -54,7 +59,6 @@ class TaskRepository:
 if __name__ == "__main__":
   task2 = Task('buy fruits', 'eating', difficulty=2)
   repo = TaskRepository()
+  repo.delete(task2)
   tasks = repo.get()
   print(tasks)
-
-
